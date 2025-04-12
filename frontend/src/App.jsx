dcf21dd873b3a58469666bc8bb6726afbfb62b8f
kelvinempire <kelvinewurum@gmail.com>
2025-04-12 14:35:49 +0100

    refactor App component to import Verify correctly and remove commented-out code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import { Contact } from "./pages/Contact";
 import Footer from "./components/Footer";
 import SearchBar from "./components/SearchBar";
 import { ToastContainer,} from "react-toastify";
-import { Verify } from "./pages/verify";
+import Verify from "./pages/verify";
 
 
 function App() {
@@ -41,36 +41,3 @@ function App() {
 
 export default App;
 
-// import { Route, Routes } from "react-router-dom";
-// import React from "react";
-// import Navbar from "./components/Navbar";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Collection from "./pages/Collection";
-// import { Contact } from "./pages/Contact";
-// import Login from "./pages/Login";
-// import PlaceOrder from "./pages/PlaceOrder";
-// import Orders from "./pages/Orders";
-// import Product from "./pages/Product";
-// import Cart from "./pages/Cart";
-
-// const App = () => {
-//   return (
-//     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] xl:px-[15vw]">
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/collection" element={<Collection />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/Place-order" element={<PlaceOrder />} />
-//         <Route path="/order" element={<Orders />} />
-//         <Route path="product/:productId" element={<Product />} />
-//         <Route path="cart" element={<Cart/>}/>
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// export default App;
