4abcc6fb8f90d2ce51e5f4736b3eb1c756be0620
kelvinempire <kelvinewurum@gmail.com>
2025-04-14 14:05:39 +0100

    refactor CORS middleware to use default settings for simplicity

--- backend/server.js
+++ backend/server.js
@@ -16,17 +16,7 @@ const PORT = process.env.PORT || 4000;
 // middleware
 
 app.use(express.json());
-const corsOptions = {
-  origin: [
-    "https://joel-hair-empire.vercel.app",
-    "https://joel-admin.vercel.app",
-  ],
-  methods: "GET, POST, PUT, DELETE",
-  allowedHeaders: "Content-Type, Authorization",
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 //api end point
 
@@ -35,14 +25,6 @@ app.use("/api/product", productRouter);
 app.use("/api/cart", cardRouter);
 app.use("/api/order", orderRouter);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-app.get("/favicon.ico", (req, res) => res.status(204).end());
 app.get("/", (req, res) => {
   res.json("app working");
 });
