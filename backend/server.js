a57a824da36e164f2d627e80b2440df7ea5888da
kelvinempire <kelvinewurum@gmail.com>
2025-04-17 17:00:03 +0100

    update CORS middleware to specify allowed headers for requests

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ app.use(
       "http://localhost:5174",
     ],
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
