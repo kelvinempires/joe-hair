843c25630c1427bc6dc9fc1d4d228d91373ffdaf
kelvinempire <kelvinewurum@gmail.com>
2025-05-18 21:44:27 +0100

    fix: update log message for successful database connection

--- backend/config/conn.js
+++ backend/config/conn.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database connected successfully");
+    console.log("Database is connected successfully");
     return mongoose.connection;
   } catch (error) {
     console.log("Error while connecting to database", error);
