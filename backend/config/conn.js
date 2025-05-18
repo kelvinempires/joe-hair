5c799a88db49ceaa8ef70ddbb0795d2f58fb3ef0
kelvinempire <kelvinewurum@gmail.com>
2025-05-18 21:57:50 +0100

    fix: update Cloudinary dependency version and improve database connection log message

--- backend/config/conn.js
+++ backend/config/conn.js
@@ -6,7 +6,7 @@ dotenv.config();
 const connectMongoDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database is connected successfully");
+    console.log("Database connected successfully");
     return mongoose.connection;
   } catch (error) {
     console.log("Error while connecting to database", error);
