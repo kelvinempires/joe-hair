c321e1eb95b05eacf778d0bfdb4feec8c70c851a
kelvinempire <kelvinewurum@gmail.com>
2025-05-18 22:10:40 +0100

    multer malfunctioning

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,21 +1,20 @@
 import multer from "multer";
 import { v2 as cloudinary } from "cloudinary";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
+import pkg from "multer-storage-cloudinary";
+const { CloudinaryStorage } = pkg;
 import "dotenv/config";
 
-// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Setup Cloudinary Storage
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "uploads", // Folder in Cloudinary
-    format: async (req, file) => "png", // Change format if needed
+    folder: "uploads",
+    format: async (req, file) => "png",
     public_id: (req, file) => file.fieldname + "-" + Date.now(),
   },
 });
